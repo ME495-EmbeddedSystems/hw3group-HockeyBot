8f1165cd2257f34211e39930c87dad3bb1bde1ed
Marno <marnonel2023@u.northwestern.edu>
2022-11-05 15:56:39 -0500

    Can plan and execute and make it display in rviz

--- moveit_helper/setup.py
+++ moveit_helper/setup.py
@@ -19,7 +19,9 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': ['move_action_intercept=intercept.move_action_intercept:interept_entry'
+        'console_scripts': ['move_action_intercept=intercept.move_action_intercept:interept_entry',
+                            'marno_move_group_plan=moveit_helper.marno_move_group_plan:interept_entry', # For Marno Test code
+                            'marno_move_group_execute=moveit_helper.marno_move_group_execute:interept_entry'
         ],
     },
 )
