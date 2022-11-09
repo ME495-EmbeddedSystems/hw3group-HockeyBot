4442e9a3235e26c239f73e785cabc902ce96a1c4
ritika <ritikaghosh2023@u.northwestern.edu>
2022-11-09 17:02:49 -0600

    added a service to get starting position

--- moveit_helper/moveit_helper/inverse.py
+++ moveit_helper/moveit_helper/inverse.py
@@ -1,7 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from moveit_msgs.msg import PositionIKRequest, JointConstraint
-from moveit_interface.srv import Goal
+from moveit_interface.srv import Goal, Initial
+from .quaternion import euler_quaternion
 from moveit_msgs.msg import PositionIKRequest
 from moveit_msgs.srv import GetPositionIK
 from enum import Enum, auto
@@ -25,6 +26,7 @@ class InverseKinematics(Node):
 
         self.state = State.INITIAL
         self.goal = self.create_service(Goal, "/goal_service", self.goal_service)
+        self.initial = self.create_service(Initial, "/initial_service", self.initial_service)
         self.ik_client = self.create_client(GetPositionIK, "compute_ik")
         self.timer = self.create_timer(0.1, self.timer_callback)
         self.rq = PositionIKRequest()
@@ -33,24 +35,32 @@ class InverseKinematics(Node):
         self.rq.group_name='panda_arm'
         self.rq.robot_state.joint_state.header.stamp=self.get_clock().now().to_msg()
         self.rq.robot_state.joint_state.header.frame_id='panda_link0'
-        self.rq.robot_state.joint_state.name=['panda_joint1', 'panda_joint2', 'panda_joint3', 'panda_joint4', 'panda_joint5', 'panda_joint6','panda_joint7' ]
+        self.rq.robot_state.joint_state.name=['panda_joint1', 'panda_joint2', 'panda_joint3',
+                                              'panda_joint4', 'panda_joint5', 'panda_joint6',
+                                              'panda_joint7' ]
         self.rq.robot_state.joint_state.position=[0.0,-0.70, 0.0, -2.35, 0.0, 1.57, 0.79]
         self.rq.robot_state.multi_dof_joint_state.header.stamp=self.get_clock().now().to_msg()
         self.rq.robot_state.multi_dof_joint_state.header.frame_id='panda_link0'
-        self.rq.robot_state.multi_dof_joint_state.joint_names=['panda_joint1', 'panda_joint2', 'panda_joint3', 'panda_joint4', 'panda_joint5', 'panda_joint6','panda_joint7' ]
+        self.rq.robot_state.multi_dof_joint_state.joint_names=['panda_joint1', 'panda_joint2', 
+                                                                'panda_joint3', 'panda_joint4', 
+                                                                'panda_joint5', 'panda_joint6',
+                                                                'panda_joint7' ]
         self.rq.robot_state.is_diff=False
         self.rq.avoid_collisions = True
         self.rq.ik_link_name = 'panda_link8'
         self.rq.pose_stamped.header.stamp = self.get_clock().now().to_msg()
         self.rq.pose_stamped.header.frame_id = 'panda_link0'
-        self.rq.pose_stamped.pose.position.x = self.pose_x
-        self.rq.pose_stamped.pose.position.y = self.pose_y
-        self.rq.pose_stamped.pose.position.z = self.pose_z
-        self.rq.pose_stamped.pose.orientation.x = self.ori_x
-        self.rq.pose_stamped.pose.orientation.y = self.ori_y
-        self.rq.pose_stamped.pose.orientation.z = self.ori_z
-        self.rq.pose_stamped.pose.orientation.w = self.ori_w
-        self.rq.ik_link_names = ['panda_hand', 'panda_hand_tcp', 'panda_leftfinger', 'panda_link0', 'panda_link1', 'panda_link2', 'panda_link3', 'panda_link4', 'panda_link5', 'panda_link6', 'panda_link7', 'panda_link8', 'panda_rightfinger']
+        self.rq.pose_stamped.pose.position.x = self.goal_x
+        self.rq.pose_stamped.pose.position.y = self.goal_y
+        self.rq.pose_stamped.pose.position.z = self.goal_z
+        self.rq.pose_stamped.pose.orientation.x = self.goal_ori_x
+        self.rq.pose_stamped.pose.orientation.y = self.goal_ori_y
+        self.rq.pose_stamped.pose.orientation.z = self.goal_ori_z
+        self.rq.pose_stamped.pose.orientation.w = self.goal_ori_w
+        self.rq.ik_link_names = ['panda_hand', 'panda_hand_tcp', 'panda_leftfinger', 'panda_link0', 
+                                    'panda_link1', 'panda_link2', 'panda_link3', 'panda_link4', 
+                                    'panda_link5', 'panda_link6', 'panda_link7', 'panda_link8',
+                                    'panda_rightfinger']
         self.rq.pose_stamped_vector = []
         self.rq.timeout.sec = 60
         self.get_logger().info(f'before async')
@@ -59,13 +69,27 @@ class InverseKinematics(Node):
 
     def goal_service(self, request, response):
         self.state = State.GETTING
-        self.pose_x=request.pose_x
-        self.pose_y=request.pose_y
-        self.pose_z=request.pose_z
-        self.ori_x=request.orientation_x
-        self.ori_y=request.orientation_y
-        self.ori_z=request.orientation_z
-        self.ori_w=request.orientation_w
+        self.goal_x=request.x
+        self.goal_y=request.y
+        self.goal_z=request.z
+        self.goal_roll=request.roll
+        self.goal_pitch=request.pitch
+        self.goal_yaw=request.yaw
+        self.goal_ori_x, self.goal_ori_y, self.goal_ori_z, self.goal_ori_w = euler_quaternion(
+                                                    self.goal_roll, self.goal_pitch, self.goal_yaw)
+        return response
+
+
+    def goal_service(self, request, response):
+        # self.state = State.GETTING
+        self.init_x=request.x
+        self.init_y=request.y
+        self.init_z=request.z
+        self.init_roll=request.roll
+        self.init_pitch=request.pitch
+        self.init_yaw=request.yaw
+        self.ini_ori_x, self.init_ori_y, self.init_ori_z, self.init_ori_w = euler_quaternion(
+                                                    self.goal_roll, self.goal_pitch, self.goal_yaw)
         return response
 
 
---
+++ moveit_helper/moveit_helper/quaternion.py
@@ -0,0 +1,21 @@
+""" Convert from angle-axis to quaternion and calculate the distance between 2 point"""
+
+from geometry_msgs.msg import Quaternion
+from math import sin, cos
+
+
+def euler_quaternion(r, p, y):
+    """ Convert from roll, pitch, yaw of rotation to a quaternion
+    Arg:
+      r: The roll (rotation around x-axis) angle in radians
+      p: The pitch (rotation around y-axis) angle in radians.
+      y: The yaw (rotation around z-axis) angle in radians
+    Return:
+      A Quaternion corresponding to the rotation
+    """
+    x = sin(r/2) * cos(p/2) * cos(y/2) - cos(r/2) * sin(p/2) * sin(y/2)
+    y = cos(r/2) * sin(p/2) * cos(y/2) + sin(r/2) * cos(p/2) * sin(y/2)
+    z = cos(r/2) * cos(p/2) * sin(y/2) - sin(r/2) * sin(p/2) * cos(y/2)
+    w = cos(r/2) * cos(p/2) * cos(y/2) + sin(r/2) * sin(p/2) * sin(y/2)
+ 
+    return x, y, z, w
