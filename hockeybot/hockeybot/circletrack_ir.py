6d36db8f34688fdf454c0aa1c59c9a0b7824209f
ritika <ritikaghosh2023@u.northwestern.edu>
2022-11-21 15:52:01 -0600

    This one detects puck in IR

--- hockeybot/hockeybot/circletrack_ir.py
+++ hockeybot/hockeybot/circletrack_ir.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 import numpy as np
 import cv2
 import pyrealsense2 as rs
@@ -11,42 +10,42 @@ pipeline_profile = config.resolve(pipeline_wrapper)
 device = pipeline_profile.get_device()
 device_product_line = str(device.get_info(rs.camera_info.product_line))
 
-found_rgb = False
+found_ir = False
 for s in device.sensors:
-    if s.get_info(rs.camera_info.name) == 'RGB Camera':
-        found_rgb = True
+    if s.get_info(rs.camera_info.name) == 'Stereo Module':
+        found_ir = True
         break
-if not found_rgb:
-    print("The demo requires camera with Color sensor")
+if not found_ir:
+    print("The demo requires camera with IR sensor")
     exit(0)
 
 # config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.infrared, 1, 480, 270,  rs.format.y8, 90)
+config.enable_stream(rs.stream.infrared, 2, 480, 270,  rs.format.y8, 90)
 
-if device_product_line == 'L500':
-    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 60)
-else:
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 60)
 
 # Start streaming
 profile = pipeline.start(config)
 
-# vid = cv2.VideoCapture("NEWvid.mkv")
 i =0
 
 try:
     while True:
         frame = pipeline.wait_for_frames()
+        ir_frame = frame.get_infrared_frame(1)
+        if not ir_frame:
+            print('something')
+            continue
+        color_image = np.asanyarray(ir_frame.get_data())
+        # cv2.namedWindow('IR Example', cv2.WINDOW_AUTOSIZE)
+        # cv2.imshow('IR Example', image)
         image = frame
         output = frame
-        color_frame = frame.get_color_frame()
-        # Validate that both frames are valid
-        if not color_frame:
-            continue
-        color_image = np.asanyarray(color_frame.get_data())
-        gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+    
+        # gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
         # detect circles in the image
         # cv2.imshow("kjadbkjad", gray)
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 2, 70, param1=200, param2=40, minRadius=17, maxRadius=25)
+        circles = cv2.HoughCircles(color_image, cv2.HOUGH_GRADIENT, 2, 70, param1=200, param2=40, minRadius=7, maxRadius=12)
         # ensure at least some circles were found
         if circles is not None:
             # convert the (x, y) coordinates and radius of the circles to integers
@@ -60,6 +59,7 @@ try:
             # show the output image
             i += 1
             print (i, x, y)
+        cv2.namedWindow('output', cv2.WINDOW_AUTOSIZE)
         cv2.imshow("output", color_image)
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q') or key == 27:
