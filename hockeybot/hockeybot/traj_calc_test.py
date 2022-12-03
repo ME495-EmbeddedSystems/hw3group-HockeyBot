ab3b93d32a60c8100e0b9dcb988d75ff82d691ec
Marno <marnonel2023@u.northwestern.edu>
2022-12-02 21:52:28 -0600

    Can do multiple impacts. Add waypoint selection

--- hockeybot/hockeybot/traj_calc_test.py
+++ hockeybot/hockeybot/traj_calc_test.py
@@ -52,12 +52,15 @@ p2 = np.array([0.0,0.0])
 """ New Var """
 # Amount of impacts
 impact_count = 0
+# y-axis intercept
+c_list = np.array([])
+# Slope
+m_list = np.array([])
 # After first impact
-Imp1 = np.array([0.0,0.0]) # Impact point
-# After second impact
-Imp2 = np.array([0.0,0.0]) # Impact point
-# After third impact
-Imp3 = np.array([0.0,0.0]) # Impact point
+ImpX = np.array([0]) # Impact point X
+ImpY = np.array([0]) # Impact point Y
+# Added 0 to start just to make same length as m_list
+
 
 """ Select trajectory type:
     + sim:
@@ -77,8 +80,12 @@ if sim == 0:
         # p1[0] = p2[0] # TODO handle situation where x1 = x2 then m = infinity (Slope)
 
         c, m = traj_puck(p1,p2)
+        m_list = np.append(m_list, m)
+        c_list = np.append(c_list, c)
         mf = m
         cf = c
+        print(f"c = {c}, m = {m}")
+        print(f"\n 1 clist = {c_list}, 1 m_list = {m_list} \n")
 
 
 
@@ -88,44 +95,60 @@ if sim == 0:
         collision = True
         while collision == True:
 
+            if impact_count == 30:
+                break
+
             y_xmin = mf*Table_xmin + cf
             y_xmax = mf*Table_xmax + cf
             print(f"y_xmin = {y_xmin}, y_xmax = {y_xmax}")
+
+            print(f"\n clist = {c_list}, m_list = {m_list} \n")
 
 
             if y_xmin < Table_ymax and y_xmin > Table_ymin and y_xmin < y_xmax:
                 impact_count += 1
 
-                if impact_count == 1:
-                    m1 = -m # Reflect impact angle
-                    # Impact coordinates
-                    Imp1[0] = Table_xmin
-                    Imp1[1] = y_xmin
-                    # Call trajectory calculation for new trajectory y-axis intercept
-                    c1 = after_impact_traj_puck(Imp1,m1)
-                    # Set current c,m equal to final cf and mf
-                    cf = c1
-                    mf = m1
-                elif impact_count == 2:
-                    m2 = -m1 # Reflect impact angle
-                    # Impact coordinates
-                    Imp2[0] = Table_xmin
-                    Imp2[1] = y_xmin
-                    # Call trajectory calculation for new trajectory y-axis intercept
-                    c2 = after_impact_traj_puck(Imp2,m2)
-                    # Set current c,m equal to final cf and mf
-                    cf = c2
-                    mf = m2
-                elif impact_count == 3:
-                    m3 = -m2 # Reflect impact angle
-                    # Impact coordinates
-                    Imp3[0] = Table_xmin
-                    Imp3[1] = y_xmin
-                    # Call trajectory calculation for new trajectory y-axis intercept
-                    c3 = after_impact_traj_puck(Imp3,m3)
-                    # Set current c,m equal to final cf and mf
-                    cf = c3
-                    mf = m3
+                m_list = np.append(m_list,-m_list[impact_count-1]) # Reflect impact angle
+                print(f"\n clist = {c_list}, m_list = {m_list} \n")
+                # Impact coordinates
+                ImpX = np.append(ImpX, Table_xmin)
+                ImpY = np.append(ImpY, y_xmin)
+                # Call trajectory calculation for new trajectory y-axis intercept
+                c_list = np.append(c_list, after_impact_traj_puck([ImpX[impact_count], ImpY[impact_count]],m_list[impact_count]))
+                # Set current c,m equal to final cf and mf
+                cf = c_list[impact_count]
+                mf = m_list[impact_count]
+
+                # if impact_count == 1:
+                #     m1 = -m # Reflect impact angle
+                #     # Impact coordinates
+                #     Imp1[0] = Table_xmin
+                #     Imp1[1] = y_xmin
+                #     # Call trajectory calculation for new trajectory y-axis intercept
+                #     c1 = after_impact_traj_puck(Imp1,m1)
+                #     # Set current c,m equal to final cf and mf
+                #     cf = c1
+                #     mf = m1
+                # elif impact_count == 2:
+                #     m2 = -m1 # Reflect impact angle
+                #     # Impact coordinates
+                #     Imp2[0] = Table_xmin
+                #     Imp2[1] = y_xmin
+                #     # Call trajectory calculation for new trajectory y-axis intercept
+                #     c2 = after_impact_traj_puck(Imp2,m2)
+                #     # Set current c,m equal to final cf and mf
+                #     cf = c2
+                #     mf = m2
+                # elif impact_count == 3:
+                #     m3 = -m2 # Reflect impact angle
+                #     # Impact coordinates
+                #     Imp3[0] = Table_xmin
+                #     Imp3[1] = y_xmin
+                #     # Call trajectory calculation for new trajectory y-axis intercept
+                #     c3 = after_impact_traj_puck(Imp3,m3)
+                #     # Set current c,m equal to final cf and mf
+                #     cf = c3
+                #     mf = m3
 
 
                 print(" Wall collision left")
@@ -133,36 +156,47 @@ if sim == 0:
             elif y_xmax < Table_ymax and y_xmax > Table_ymin and y_xmax < y_xmin:
                 impact_count += 1
 
-                if impact_count == 1:
-                    m1 = -m # Reflect impact angle
-                    # Impact coordinates
-                    Imp1[0] = Table_xmax
-                    Imp1[1] = y_xmax
-                    # Call trajectory calculation for new trajectory y-axis intercept
-                    c1 = after_impact_traj_puck(Imp1,m1)
-                    # Set current c,m equal to final cf and mf
-                    cf = c1
-                    mf = m1
-                elif impact_count == 2:
-                    m2 = -m1 # Reflect impact angle
-                    # Impact coordinates
-                    Imp2[0] = Table_xmin
-                    Imp2[1] = y_xmin
-                    # Call trajectory calculation for new trajectory y-axis intercept
-                    c2 = after_impact_traj_puck(Imp2,m2)
-                    # Set current c,m equal to final cf and mf
-                    cf = c2
-                    mf = m2
-                elif impact_count == 3:
-                    m3 = -m2 # Reflect impact angle
-                    # Impact coordinates
-                    Imp3[0] = Table_xmin
-                    Imp3[1] = y_xmin
-                    # Call trajectory calculation for new trajectory y-axis intercept
-                    c3 = after_impact_traj_puck(Imp3,m3)
-                    # Set current c,m equal to final cf and mf
-                    cf = c3
-                    mf = m3
+                m_list = np.append(m_list,-m_list[impact_count-1]) # Reflect impact angle
+                print(f"\n clist = {c_list}, m_list = {m_list} \n")
+                 # Impact coordinates
+                ImpX = np.append(ImpX, Table_xmax)
+                ImpY = np.append(ImpY, y_xmax)
+                # Call trajectory calculation for new trajectory y-axis intercept
+                c_list = np.append(c_list, after_impact_traj_puck([ImpX[impact_count], ImpY[impact_count]],m_list[impact_count]))
+                # Set current c,m equal to final cf and mf
+                cf = c_list[impact_count]
+                mf = m_list[impact_count]
+
+                # if impact_count == 1:
+                #     m1 = -m # Reflect impact angle
+                #     # Impact coordinates
+                #     Imp1[0] = Table_xmax
+                #     Imp1[1] = y_xmax
+                #     # Call trajectory calculation for new trajectory y-axis intercept
+                #     c1 = after_impact_traj_puck(Imp1,m1)
+                #     # Set current c,m equal to final cf and mf
+                #     cf = c1
+                #     mf = m1
+                # elif impact_count == 2:
+                #     m2 = -m1 # Reflect impact angle
+                #     # Impact coordinates
+                #     Imp2[0] = Table_xmin
+                #     Imp2[1] = y_xmin
+                #     # Call trajectory calculation for new trajectory y-axis intercept
+                #     c2 = after_impact_traj_puck(Imp2,m2)
+                #     # Set current c,m equal to final cf and mf
+                #     cf = c2
+                #     mf = m2
+                # elif impact_count == 3:
+                #     m3 = -m2 # Reflect impact angle
+                #     # Impact coordinates
+                #     Imp3[0] = Table_xmin
+                #     Imp3[1] = y_xmin
+                #     # Call trajectory calculation for new trajectory y-axis intercept
+                #     c3 = after_impact_traj_puck(Imp3,m3)
+                #     # Set current c,m equal to final cf and mf
+                #     cf = c3
+                #     mf = m3
 
                 print(" Wall collision right")
                 collision = True # TODO: Add back in so that collisions will keep on be corrected
@@ -170,7 +204,7 @@ if sim == 0:
             else: # No collision
                 collision = False
 
-
+        print(f"clist = {c_list}, m_list = {m_list}")
         wx1, wx2 = play_waypoints(cf,mf)
 
         if wx1 > xmin and wx1 < xmax:
@@ -210,29 +244,37 @@ if sim == 0:
         plt.axline((p1[0], p1[1]), slope=m, color="blue", linestyle=(0, (5, 5)), label = 'Puck trajectory line')
 
 
-        if impact_count >= 1:
-            # Impact point 1
-            plt.plot([Imp1[0]],[Imp1[1]], 'ro', color = 'blue', label = 'Impact point')
-            # New after impact trajectory line
-            plt.axline((Imp1[0],Imp1[1]), slope=m1, color="red", linestyle=(0, (5, 5)), label = 'New puck trajectory line')
-            # Trajectory - Y-axis intercect
-            plt.plot([0],[c1], 'x', color = 'red', label = 'Traj1 intercect with y-axis')
-            if impact_count >= 2:
-                print("SECOND HIT")
-                # Impact point 2
-                plt.plot([Imp2[0]],[Imp2[1]], 'ro', color = 'blue', label = 'Impact point')
+        # if impact_count >= 1:
+        #     # Impact point 1
+        #     plt.plot([Imp1[0]],[Imp1[1]], 'ro', color = 'blue', label = 'Impact point')
+        #     # New after impact trajectory line
+        #     plt.axline((Imp1[0],Imp1[1]), slope=m1, color="red", linestyle=(0, (5, 5)), label = 'New puck trajectory line')
+        #     # Trajectory - Y-axis intercect
+        #     plt.plot([0],[c1], 'x', color = 'red', label = 'Traj1 intercect with y-axis')
+        #     if impact_count >= 2:
+        #         print("SECOND HIT")
+        #         # Impact point 2
+        #         plt.plot([Imp2[0]],[Imp2[1]], 'ro', color = 'blue', label = 'Impact point')
+        #         # New after impact trajectory line
+        #         plt.axline((Imp2[0],Imp2[1]), slope=m2, color="red", linestyle=(0, (5, 5)), label = 'New puck trajectory line')
+        #         # Trajectory - Y-axis intercect
+        #         plt.plot([0],[c2], 'x', color = 'red', label = 'Traj1 intercect with y-axis')
+        #         if impact_count >= 3:
+        #             print("THIRD HIT")
+        #             # Impact point 2
+        #             plt.plot([Imp3[0]],[Imp3[1]], 'ro', color = 'blue', label = 'Impact point')
+        #             # New after impact trajectory line
+        #             plt.axline((Imp3[0],Imp3[1]), slope=m3, color="red", linestyle=(0, (5, 5)), label = 'New puck trajectory line')
+        #             # Trajectory - Y-axis intercect
+        #             plt.plot([0],[c3], 'x', color = 'red', label = 'Traj1 intercect with y-axis')
+        if len(c_list) != 0:
+            for imp in range(1,len(c_list)):
+                # Impact point 1
+                plt.plot([ImpX[imp]],[ImpY[imp]], 'ro', color = 'blue', label = 'Impact point')
                 # New after impact trajectory line
-                plt.axline((Imp2[0],Imp2[1]), slope=m2, color="red", linestyle=(0, (5, 5)), label = 'New puck trajectory line')
+                plt.axline((ImpX[imp],ImpY[imp]), slope=m_list[imp], color="red", linestyle=(0, (5, 5)), label = 'New puck trajectory line')
                 # Trajectory - Y-axis intercect
-                plt.plot([0],[c2], 'x', color = 'red', label = 'Traj1 intercect with y-axis')
-                if impact_count >= 3:
-                    print("THIRD HIT")
-                    # Impact point 2
-                    plt.plot([Imp3[0]],[Imp3[1]], 'ro', color = 'blue', label = 'Impact point')
-                    # New after impact trajectory line
-                    plt.axline((Imp3[0],Imp3[1]), slope=m3, color="red", linestyle=(0, (5, 5)), label = 'New puck trajectory line')
-                    # Trajectory - Y-axis intercect
-                    plt.plot([0],[c3], 'x', color = 'red', label = 'Traj1 intercect with y-axis')
+                plt.plot([0],[c_list[imp]], 'x', color = 'red', label = 'Traj1 intercect with y-axis')
 
 
 
