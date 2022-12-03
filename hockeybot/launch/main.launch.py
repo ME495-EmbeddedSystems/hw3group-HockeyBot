1ac14ffe790a146a9db5ad0294e872751ffd8575
Hanyin Yuan <hanyinyuan2024@u.northwestern.edu>
2022-12-03 13:24:25 -0600

    add rviz in launch file

--- hockeybot/launch/main.launch.py
+++ hockeybot/launch/main.launch.py
@@ -2,9 +2,68 @@
 """Launch file for Franka robot."""
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument,Shutdown,ExecuteProcess,IncludeLaunchDescription
+from launch.conditions import LaunchConfigurationEquals,IfCondition,UnlessCondition
+from launch.substitutions import LaunchConfiguration,PathJoinSubstitution,Command,FindExecutable
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+import yaml
+import os
 
 def generate_launch_description():
     """Launch node."""
+    # Franka = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+    #     [os.path.join(get_package_share_directory('franka_moveit_config')),
+    #     '/launch/moveit.launch.py']),
+    # launch_arguments={
+    #             'robot_ip': 'dont care',
+
+    #         }.items())
+    # use_fake_hardware = DeclareLaunchArgument(
+    #     name = 'use_fake_hardware',
+    #     default_value = 'false',
+    #     choices= [
+    #         'false',
+    #         'true',
+    #         'none'],
+    #     description = 'flag to choose use Franka or Rviz'
+    # )
+    
+    # ros2_control_node = Node(
+    #     package='controller_manager',
+    #     executable='ros2_control_node',
+    #      parameters=[robot_description, ros2_controllers_path],
+    #     remappings=[('joint_states', 'franka/joint_states')],
+    #     condition=LaunchConfigurationEquals('use_fake_hardware','false'),
+    #             output={
+    #         'stdout': 'screen',
+    #         'stderr': 'screen',
+    #     },
+    # )
+    # ros2_control_node = Node(
+    #     package='controller_manager',
+    #     executable='ros2_control_node',
+    #     parameters=[robot_description, ros2_controllers_path],
+    #     remappings=[('joint_states', 'franka/joint_states')],
+    #     condition=LaunchConfigurationEquals('use_fake_hardware','true'),
+    #             output={
+    #         'stdout': 'screen',
+    #         'stderr': 'screen',
+    #     },
+    #     on_exit=Shutdown(),
+    # )
+    # ros2_control_node = Node(
+    #     package='controller_manager',
+    #     executable='ros2_control_node',
+    #      parameters=[robot_description, ros2_controllers_path],
+    #     remappings=[('joint_states', 'franka/joint_states')],
+    #     condition=LaunchConfigurationEquals('use_fake_hardware','none'),
+    #             output={
+    #         'stdout': 'screen',
+    #         'stderr': 'screen',
+    #     },
+    # )
     main = Node(
         package='hockeybot',
         executable='main',
@@ -29,5 +88,10 @@ def generate_launch_description():
         main,
         Camera,
         Trajectory,
-        simple_move_node
+        simple_move_node,
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(
+        [os.path.join(get_package_share_directory('franka_moveit_config')),
+        '/launch/moveit.launch.py']),
+        launch_arguments={'robot_ip': []}.items(),
+        )
     ])
