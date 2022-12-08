0c647c8a5ce3f30830551bef2ef5af5d005b5f03
Marno <marnonel2023@u.northwestern.edu>
2022-12-08 15:55:44 -0600

    Deleted unused files

--- hockeybot/apriltag_ros-master/src/tag_functions.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <apriltag.h>
-#include <map>
-#include <string>
-
-extern const std::map<std::string, apriltag_family_t *(*)(void)> tag_create;
-
-extern const std::map<std::string, void (*)(apriltag_family_t*)> tag_destroy;
