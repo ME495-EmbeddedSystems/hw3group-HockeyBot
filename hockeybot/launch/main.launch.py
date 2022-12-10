39472c1385179c77687115d90db9122ae3dc668d
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2022-12-10 17:27:46 -0600

    Style errors fixed for main launch

--- hockeybot/launch/main.launch.py
+++ hockeybot/launch/main.launch.py
@@ -1,60 +1,77 @@
-
 """Launch file for Franka robot."""
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument,Shutdown,ExecuteProcess,IncludeLaunchDescription
-from launch.conditions import LaunchConfigurationEquals,IfCondition,UnlessCondition
-from launch.substitutions import LaunchConfiguration,PathJoinSubstitution,Command,FindExecutable, PythonExpression
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import LaunchConfigurationEquals
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.substitutions import FindPackageShare
-import yaml
 import os
+
 
 def generate_launch_description():
     """Launch node and moveit/rviz."""
-    robot = DeclareLaunchArgument(name = 'robot', 
-    				  default_value='none',
-    				  choices = ['true',
-    				  'false',
-    				  'none'],
-                                  description='Determines if moveit.launch.py is called.')
+    DeclareLaunchArgument(
+        name="robot",
+        default_value="none",
+        choices=["true", "false", "none"],
+        description="Determines if moveit.launch.py is called.",
+    )
     main = Node(
-        package='hockeybot',
-        executable='main',
-        name='main_node',
+        package="hockeybot",
+        executable="main",
+        name="main_node",
     )
     Camera = Node(
-        package='hockeybot',
-        executable='cam_node',
-        name='cam_node',
+        package="hockeybot",
+        executable="cam_node",
+        name="cam_node",
     )
     Trajectory = Node(
-        package='hockeybot',
-        executable='traj_calc',
-        name='traj_node',
+        package="hockeybot",
+        executable="traj_calc",
+        name="traj_node",
     )
     simple_move_node = Node(
-        package='moveit_helper',
-        executable='simple_move',
-        name='simple_mode',
+        package="moveit_helper",
+        executable="simple_move",
+        name="simple_mode",
     )
-    return LaunchDescription([
-        main,
-        Camera,
-        Trajectory,
-        simple_move_node,
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(
-        [os.path.join(get_package_share_directory('franka_moveit_config')),
-        '/launch/moveit.launch.py']),
-        launch_arguments={'robot_ip': 'dont-care','use_fake_hardware': 'true'}.items(),
-        condition = LaunchConfigurationEquals('robot', 'false') #launch moveit.launch.py
-        ),
-
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(
-        [os.path.join(get_package_share_directory('franka_moveit_config')),
-        '/launch/rviz.launch.py']),
-        launch_arguments={'robot_ip': 'panda0.robot'}.items(),
-        condition = LaunchConfigurationEquals('robot', 'true') #launch rviz.launch.py
-        )
-    ])
+    return LaunchDescription(
+        [
+            main,
+            Camera,
+            Trajectory,
+            simple_move_node,
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        os.path.join(
+                            get_package_share_directory("franka_moveit_config")
+                        ),
+                        "/launch/moveit.launch.py",
+                    ]
+                ),
+                launch_arguments={
+                    "robot_ip": "dont-care",
+                    "use_fake_hardware": "true",
+                }.items(),
+                condition=LaunchConfigurationEquals(
+                    "robot", "false"
+                ),  # launch moveit.launch.py
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        os.path.join(
+                            get_package_share_directory("franka_moveit_config")
+                        ),
+                        "/launch/rviz.launch.py",
+                    ]
+                ),
+                launch_arguments={"robot_ip": "panda0.robot"}.items(),
+                condition=LaunchConfigurationEquals(
+                    "robot", "true"
+                ),  # launch rviz.launch.py
+            ),
+        ]
+    )
